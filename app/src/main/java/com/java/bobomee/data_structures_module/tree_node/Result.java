dae01e729431a2f09b3fe70c193ff82647f3c41e
BoBoMEe <wbwjx115@gmail.com>
2017-08-21 10:38:06 +0800

    Initial commit

---
+++ app/src/main/java/com/java/bobomee/data_structures_module/tree_node/Result.java
@@ -0,0 +1,24 @@
+package com.java.bobomee.data_structures_module.tree_node;
+
+/**
+ * Project ID：400YF17051<br/>
+ * Resume:
+ *
+ * @author 汪波
+ * @version 1.0
+ * @see
+ * @since 2017/8/13 汪波 first commit
+ */
+public class Result {
+
+  int maxDistance;
+  int maxDepth;
+
+  public Result() {
+  }
+
+  public Result(int maxDistance, int maxDepth) {
+    this.maxDistance = maxDistance;
+    this.maxDepth = maxDepth;
+  }
+}
